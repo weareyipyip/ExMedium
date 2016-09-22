83dddf9eee9d2ab467bb9de6ffcfa032c2480e69
Evert Verboven <e.verboven@yipyip.nl>
2016-09-22 15:22:15 +0200

    updated version 0.1.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExMedium.Mixfile do
 
   def project do
     [app: :ex_medium,
-     version: "0.1.4",
+     version: "0.1.5",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
